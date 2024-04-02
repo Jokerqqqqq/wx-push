fc8a44a5e7b0eb1e4420d91429ada3166a0d9ad6
Jokerqqqqq <131391698+Jokerqqqqq@users.noreply.github.com>
2024-04-02 09:26:44 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx5d79d599e0f88948",
   appsecret: "2563b7e1d41a395c4ecee4ab2fa54d01",
   template_id: "1UW5TvTTjhwZdVP6WRtRo6pV_ZpCpjkPMnGCa8bweLA",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "olK286E2qZap_U_wo5BBJn5fW4lA",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "修水",
